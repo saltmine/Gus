8fe8060314b02110c0d3947149fc3f83498ae385
Mark Tozzi <mark.tozzi@gmail.com>
2013-10-02 18:05:24 -0400

    Schema for tracking projects & builds

--- gus/sql/bootstrap.sql
+++ gus/sql/bootstrap.sql
@@ -17,12 +17,64 @@ CREATE SEQUENCE deploy_id_seq
     NO MAXVALUE
     CACHE 1;
 
-CREATE TABLE deploys (
-  deploy_id integer NOT NULL DEFAULT nextval('deploy_id_seq'),
-  delpoy_date timestamp without time zone NOT NULL DEFAULT now(),
-  project varchar(256),
-  version varchar(256),
-  deploying_user varchar(256),
-  CONSTRAINT deploy_pkey PRIMARY KEY (deploy_id)
+CREATE SEQUENCE project_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE release_candidate_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE environment_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE projects (
+  project_id integer NOT NULL
+      DEFAULT nextval('project_id_seq')
+      PRIMARY KEY,
+  project_name varchar(255),
+  date_created timestamp without time zone NOT NULL DEFAULT now()
 );
-CREATE INDEX deploy_project_date ON deploys (project, deploy_date);
+
+CREATE TABLE environments (
+  environment_id integer NOT NULL
+      DEFAULT nextval('environment_id_seq')
+      PRIMARY KEY,
+  name varchar(255),
+  hipchat_alert boolean
+);
+
+CREATE TABLE release_candidates (
+  release_candidate_id integer NOT NULL
+      DEFAULT nextval('release_candidate_id_seq')
+      PRIMARY KEY,
+  date_created timestamp without time zone NOT NULL DEFAULT now(),
+  project_id integer NOT NULL REFERENCES projects (project_id),
+  branch_name varchar(255) NOT NULL,
+  revision_id varchar(255) NOT NULL,
+  code_tarball_location text NOT NULL,
+  venv_tarball_location text NOT NULL
+);
+
+CREATE TABLE deploys (
+  deploy_id integer NOT NULL
+      DEFAULT nextval('deploy_id_seq')
+      PRIMARY KEY,
+  deploy_date timestamp without time zone NOT NULL DEFAULT now(),
+  project_id integer NOT NULL REFERENCES projects (project_id),
+  release_candidate_id integer NOT NULL
+      REFERENCES release_candidates (release_candidate_id),
+  environment_id integer NOT NULL REFERENCES environments (environment_id),
+  deploying_user varchar(256)
+);
+CREATE INDEX deploy_project_date ON deploys (project_id, deploy_date);
